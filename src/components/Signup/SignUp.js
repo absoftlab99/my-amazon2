3aa9624ce559bd7af4115fdb4fd428b103b13066
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-10-20 12:54:00 +0600

    login and signup component setup with routers

---
+++ src/components/Signup/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div className='pt-5 text-center'>
+            <h1>this is signup page</h1>
+        </div>
+    );
+};
+
+export default SignUp;
